507e77d0a9ce58e9fafd10e8446aa8fb6f6e195c
Fabian Likam <fabiansang@gmail.com>
2024-03-19 14:51:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+
+//const welcomeMessage = 'Welcome to Flatbook!';
+//const excitedWelcomeMessage ='WELCOME TO FLATBOOK';
+
+//const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+//const excitedWelcomeMessage = welcomeMessage.toLowerCase();
+
+//const shortGreeting = `welcome,  ${currentUser.slice (0, 1)}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`
+
+//const excitedWelcomeMessage = 'Welcome, $'
+
+//const excitedWelcomeMessage
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0).toUpperCase()}!`;
